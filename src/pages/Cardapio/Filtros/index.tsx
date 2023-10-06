82ed62a920c582ecdec2aa16403f5f81e738c5f9
souorichard <elaine.ra96@gmail.com>
2023-10-06 14:13:12 -0300

    Click do filtro

--- src/pages/Cardapio/Filtros/index.tsx
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -2,17 +2,25 @@ import styles from './Filtros.module.scss';
 import filtros from './filtros.json';
 
 
-type IOpcao = typeof filtros [0];
+type IOpcao = typeof filtros[0];
 
-export default function Filtros() { 
+type FiltrosProps = {
+    filtro: number | null;
+    setFiltro: React.Dispatch<React.SetStateAction<number | null>>;
+}
+
+export default function Filtros({filtro, setFiltro}: FiltrosProps) {
+    function selectFilter(option: IOpcao) {
+        return setFiltro(option.id);
+      }
+
     return (
         <div className={styles.filtro}>
             {filtros.map((opcao)  => (
-                <button key={opcao.id} className={styles.filtro__filtro}>
+                <button key={opcao.id} onClick={() => selectFilter(opcao)} className={`${styles.filtro__filtro} ${filtro === opcao.id ? styles['filtro__filtro--ativo'] : ""}`}>
                     {opcao.label}
                 </button>
-            )) }
-            
+            ))}
         </div>
     )
 }
